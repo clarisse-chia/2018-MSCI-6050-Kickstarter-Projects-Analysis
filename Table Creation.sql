7d5ca3cf595ec77ec1a67d76b3708997af98bdfa
hvahdat <42591321+hvahdat@users.noreply.github.com>
2018-11-02 16:46:26 -0500

    Update Table Creation.sql

--- Table Creation.sql
+++ Table Creation.sql
@@ -1,5 +1,8 @@
+--hutan altered data types to allow proper loading of data, date data types have been replaced with char, this shouldn't be an issue 
+--we basically changed number data types to strings for simplicity
+
 CREATE TABLE CALENDAR_T (
-    Dates date NOT NULL,
+    Dates varchar2(15) NOT NULL,
     Year varchar2(4),
     Month varchar2(2),
     DayOfMonth varchar2(2),
@@ -18,30 +21,30 @@ CREATE TABLE CALENDAR_T (
 CREATE TABLE CATEGORY_T (   
     CategoryID  varchar(4) NOT NULL,
     MainCategory varchar2(12),
-    Subcategory number(18),
+    Subcategory number(50),
     CONSTRAINT CATEGORY_PK primary key (CategoryID)
 ) ;
 
 CREATE TABLE COUNTRY_T (   
-    CountryCode  varchar(2) NOT NULL,
+    CountryCode  varchar(4) NOT NULL,
     CountryName varchar2(44),
     CONSTRAINT COUNTRY_PK primary key (CountryCode)
 ) ;
 
 CREATE TABLE PROJECT_T (
-    ProjectID varchar2(7) NOT NULL,
-    ProjectName varchar2(100),
-    CategoryID varchar2(4),
-    CountryCode varchar(2),
-    OriginalCurrency char(30),
-    LaunchDate date,
-    Deadline date,
-    ActiveDays number(2),
-    GoalAmount number(9,2),
-    PledgedAmount number(8,2),
-    ProportionReached number(6,2),
-    Backers number(6),
-    ProjectState char(10),
+    ProjectID varchar2(10) NOT NULL,
+    ProjectName varchar2(200),
+    CategoryID varchar2(10),
+    CountryCode varchar(11),
+    OriginalCurrency char(16),
+    LaunchDate varchar2(25),
+    Deadline varchar2(25),
+    ActiveDays number(10,0),
+    GoalAmount number(11,2),
+    PledgedAmount number(13,2),
+    ProportionReached number(17,2),
+    Backers number(7,0),
+    ProjectState char(12),
     CONSTRAINT PROJECT_PK primary key (ProjectID),
     CONSTRAINT LAUNCHDATE_FK foreign key (LaunchDate) references CALENDAR_T(Dates),
     CONSTRAINT DEADLINE_FK foreign key (Deadline) references CALENDAR_T(Dates),
